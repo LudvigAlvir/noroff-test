a3a38e9730357e7ed4ae95c17f084ff158f854db
Ludvig Alvir <ludvig.alvir@gmail.com>
2024-08-02 15:13:43 +0200

    test workflow

--- tests/playing.test.js
+++ tests/playing.test.js
@@ -1,1 +1,18 @@
 const { test, expect } = require("@playwright/test");
+
+test("Checking for heading", async ({ page }) => {
+	await page.goto("http://127.0.0.1:3000");
+
+	// Expect a title "to contain" a substring.
+	await expect(page.getByRole("heading", { name: "hey" })).toBeVisible();
+});
+
+test("Checking for heading that should not be there", async ({ page }) => {
+	await page.goto("http://127.0.0.1:3000");
+
+	// Expect a title "to contain" a substring.
+	const heading = await page
+		.getByRole("heading", { name: "heyheyhey" })
+		.isVisible();
+	expect(heading).toBeFalsy();
+});
